906a877ebe2efd703e2caf85a53b058ecff4cbd3
V Harshini <harshinivk12@gmail.com>
2024-06-17 19:40:05 +0530

    dashboard updates

--- client/src/components/Feedback/Feedback2.jsx
+++ client/src/components/Feedback/Feedback2.jsx
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import React, { useEffect } from "react";
 import Nav from "../Builder/Nav";
-=======
-import React from "react";
->>>>>>> c661061009eb80dd4d82601f0857f856806e0348
 import Footer from "../Footer";
 import '../Feedback/Feedback2.css';
 import checked from '../../assets/images/checked.png';
@@ -16,7 +12,6 @@ export default function Feedback2() {
 
     return (
         <>
-<<<<<<< HEAD
         <Nav />
     <div className="feed-2">
         <div class="checklogo">
@@ -31,19 +26,6 @@ export default function Feedback2() {
         <div class="bigtext2">
             <p id="greenB">It’s great to hear from you!</p>
         </div>
-=======
-    <div class="checklogo">
-        <img src={checked} class="clogo" alt=""/>
-    </div>
-    <div class="smalltext">
-        <p id="greenS">Your feedback has been submitted!</p>
-    </div>
-    <div class="bigtext1">
-        <p id="greenB">Thank you for dropping your</p>
-    </div>
-    <div class="bigtext2">
-        <p id="greenB">feedback! It’s great to hear from you!</p>
->>>>>>> c661061009eb80dd4d82601f0857f856806e0348
     </div>
     <Footer />
         </>
