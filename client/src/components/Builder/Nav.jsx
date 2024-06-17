039574a919faf1f521ca725f249ae818b6d8def2
amit-vis <amit200698@gmail.com>
2024-06-17 11:16:35 +0530

    route changes

--- client/src/components/Builder/Nav.jsx
+++ client/src/components/Builder/Nav.jsx
@@ -9,34 +9,39 @@ export default function Nav() {
     const {data, checkexpiry} = useDashboard();
     return (
         <>
-        <div className="nav">
-            <nav class="navbar navbar-expand-lg">
-                <div class="container-fluid w-100">
-                    <div className="fit-content plain-bar">
-                        <Link to={'/'}>
-                            <div className="logo-plain">
-                                <img src={logo} alt="resunova-logo" />
-                                <a class="navbar-brand brand fit-content" href="#">ResuNova</a>
-                            </div>
-                        </Link>
-                        <span>{data?.name}</span>
-                        <div className="right-nav">
-                            <button class="navbar-toggler"
-                            type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="navbar-toggler-icon"></span>
-                            </button>
-                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                                <button type="button" onClick={checkexpiry}><GetStartedBtn extraClass="sign-out" btn="Sign out" /></button>
-                                <Link to={"/profile"}><img src={profile} alt="profile" /></Link>
-                            <div class="collapse navbar-collapse" id="navbarSupportedContent" onClick={checkexpiry}>
-                                <Link to={'/signin'}><GetStartedBtn extraClass="sign-out" btn="Sign out"  /></Link>
-                                <Link to={'/profile'}><img src={profile} alt="profile" /></Link>
+            <div className="nav">
+                <nav className="navbar navbar-expand-lg">
+                    <div className="container-fluid w-100">
+                        <div className="fit-content plain-bar">
+                            <Link to={'/'}>
+                                <div className="logo-plain">
+                                    <img src={logo} alt="resunova-logo" />
+                                    <a className="navbar-brand brand fit-content" href="#">ResuNova</a>
+                                </div>
+                            </Link>
+                            <span>{data?.name}</span>
+                            <div className="right-nav">
+                                <button className="navbar-toggler"
+                                        type="button" data-bs-toggle="collapse"
+                                        data-bs-target="#navbarSupportedContent"
+                                        aria-controls="navbarSupportedContent"
+                                        aria-expanded="false"
+                                        aria-label="Toggle navigation">
+                                    <span className="navbar-toggler-icon"></span>
+                                </button>
+                                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                                    <button type="button" onClick={checkexpiry}>
+                                        <GetStartedBtn extraClass="sign-out" btn="Sign out" />
+                                    </button>
+                                    <Link to={"/profile"}>
+                                        <img src={profile} alt="profile" />
+                                    </Link>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </nav>
-        </div>
+                </nav>
+            </div>
         </>
     )
 }
